49f7dd07ddda0df1ddb83aef415041cb60af6d74
Cameron Drysdale <cameron.drysdale@thefloow.com>
2022-07-18 09:46:51 +0100

    Updates

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  target: 'serverless',
+  trailingSlash: true,
+  env: {
+    ENVIRONMENT: process.env.ENVIRONMENT,
+    BUILD_TIME: new Date().toString()
+  }
 }
 
 module.exports = nextConfig
